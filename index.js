b8a27c326bb734dda590e757da7c4bfa1c096ee2
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-06-27 20:20:27 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowercase();
+  }
+function logshout(string) {
+    return string.toUpperCase();
+  }
+function logWhisper(string) {
+    return string.toLowercase();
+  }
